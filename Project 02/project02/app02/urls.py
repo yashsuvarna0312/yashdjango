4db27f8831c6879a17c48e8692d6af60781f0d7b
yashsuvarna0312 <yashsuvarna0312@gmail.com>
2025-06-12 15:45:39 +0530

    Add files via upload

---
+++ Project 02/project02/app02/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.employee_form, name='employee_form'),
+    path('list/', views.employee_list, name='employee_list'),
+]
